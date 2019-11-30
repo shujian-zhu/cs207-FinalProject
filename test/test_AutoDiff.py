dd932e7e3dc02039d4abcea43fe59066f3d88a4f
malikwagih <mwagih@mit.edu>
2019-11-30 18:51:12 -0500

    updated tests2

--- test/test_AutoDiff.py
+++ test/test_AutoDiff.py
@@ -4,94 +4,94 @@ from AutoDiff import autodiff as ad
 
 def test_negation():
     ad1 = ad.AD_eval('-x', "x", 2)
-    assert (ad1.der, ad1.val) == (-1, -2)
+    assert (ad1.derivative('x'), ad1.val) == (-1, -2)
 
 def test_add():
     ad1 = ad.AD_eval('x + 2', "x", 2)
-    assert (ad1.der, ad1.val) == (1,4)
+    assert (ad1.derivative('x'), ad1.val) == (1,4)
 
 def test_add2():
     ad1 = ad.AD_eval('x + x', "x", 2)
-    assert (ad1.der, ad1.val) == (2,4)
+    assert (ad1.derivative('x'), ad1.val) == (2,4)
 
 def test_radd():
     ad1 = ad.AD_eval('2 + x', "x", 2)
-    assert (ad1.der, ad1.val) == (1,4)
+    assert (ad1.derivative('x'), ad1.val) == (1,4)
     
 def test_sub():
     ad1 = ad.AD_eval('x - 2', "x", 2)
-    assert (ad1.der, ad1.val) == (1,0)
+    assert (ad1.derivative('x'), ad1.val) == (1,0)
 
 def test_sub2():
     ad1 = ad.AD_eval('x - 2*x', "x", 2)
-    assert (ad1.der, ad1.val) == (-1,-2)
+    assert (ad1.derivative('x'), ad1.val) == (-1,-2)
 
 def test_rsub():
     ad1 = ad.AD_eval('2 - x', "x", 2)
-    assert (ad1.der, ad1.val) == (-1,0)
+    assert (ad1.derivative('x'), ad1.val) == (-1,0)
 
 def test_mul():
     ad1 = ad.AD_eval('x * 2', 'x', 2)
-    assert (ad1.der, ad1.val) == (2,4)
+    assert (ad1.derivative('x'), ad1.val) == (2,4)
 
 def test_mul2():
     ad1 = ad.AD_eval('x * x', 'x', 2)
-    assert (ad1.der, ad1.val) == (4,4)
+    assert (ad1.derivative('x'), ad1.val) == (4,4)
 
 def test_rmul():
     ad1 = ad.AD_eval('2 * x', 'x', 2)
-    assert (ad1.der, ad1.val) == (2,4)
+    assert (ad1.derivative('x'), ad1.val) == (2,4)
 
 def test_truediv():
     ad1 = ad.AD_eval('x/2', 'x', 2)
-    assert (ad1.der, ad1.val) == (1/2,1)
+    assert (ad1.derivative('x'), ad1.val) == (1/2,1)
 
 def test_truediv2():
     ad1 = ad.AD_eval('(x+2)/x', 'x', 2)
-    assert (ad1.der, ad1.val) == (-1/2,2)
+    assert (ad1.derivative('x'), ad1.val) == (-1/2,2)
 
 def test_rtruediv():
     ad1 = ad.AD_eval('3/x', 'x', 2)
-    assert (ad1.der, ad1.val) == (-3/4,3/2)
+    assert (ad1.derivative('x'), ad1.val) == (-3/4,3/2)
 
 def test_pow():
     ad1 = ad.AD_eval('x**3', 'x', 2)
-    assert (ad1.der, ad1.val) == (12,8)
+    assert (ad1.derivative('x'), ad1.val) == (12,8)
 def test_pow2():
     ad1 = ad.AD_eval('x**x','x',2)
-    assert (ad1.der, ad1.val) == (4+ 4*math.log(2),4)
+    assert (ad1.derivative('x'), ad1.val) == (4+ 4*math.log(2),4)
 
 def test_rpow():
     ad1 = ad.AD_eval('3**x','x', 2)
-    assert (ad1.der, ad1.val) == (9*math.log(3),9)
+    assert (ad1.derivative('x'), ad1.val) == (9*math.log(3),9)
     
 def test_rpow2():
     ad1 = ad.AD_eval('0**x','x', 2)
-    assert (ad1.der, ad1.val) == (0,0)
+    assert (ad1.derivative('x'), ad1.val) == (0,0)
     
 def test_powerrule():
     ad1 = ad.AD_eval('x**(x+1)','x', 0)
-    assert (ad1.der, ad1.val) == (0,0)
+    assert (ad1.derivative('x'), ad1.val) == (0,0)
     
 def test_exp():
     ad1 = ad.AD_eval('e(x)','x', 2)
-    assert (ad1.der, ad1.val) == (math.exp(2),math.exp(2))
+    assert (ad1.derivative('x'), ad1.val) == (math.exp(2),math.exp(2))
 
 def test_ln():
     ad1 = ad.AD_eval('ln(x)','x', 2)
-    assert (ad1.der, ad1.val) == (0.5, math.log(2))
+    assert (ad1.derivative('x'), ad1.val) == (0.5, math.log(2))
 
 def test_sin():
     ad1 = ad.AD_eval('sin(x)','x', 2)
-    assert (ad1.der, ad1.val) == (math.cos(2), math.sin(2))
+    assert (ad1.derivative('x'), ad1.val) == (math.cos(2), math.sin(2))
 
 def test_cos():
     ad1 = ad.AD_eval('cos(x)','x', 2)
-    assert (ad1.der, ad1.val) == (-math.sin(2), math.cos(2))
+    assert (ad1.derivative('x'), ad1.val) == (-math.sin(2), math.cos(2))
 
 def test_tan():
     ad1 = ad.AD_eval('tan(x)','x', 2)
-    assert (ad1.der, ad1.val) == (1/ math.cos(2)**2, math.tan(2))
+    assert (ad1.derivative('x'), ad1.val) == (1/ math.cos(2)**2, math.tan(2))
     
 def test_input_function_types():
     x = 2
