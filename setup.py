f3afb5b80057526ed1e9c36c42062eb12b771872
malikwagih <mwagih@mit.edu>
2019-12-09 23:31:51 -0500

    changed setup.py -- PyPi

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='AutoDiff',
-    version='0.1dev',
+setuptools.setup(
+    name='AutoDiff-CS207-24',
+    version='0.2',
     description='Auto Differentiation package developed for CS-207 [2019]',
     author='Group 24',
     packages=['AutoDiff',],
     license='MIT',
     long_description=open('README.md').read(),
+    url="https://github.com/we-the-diff/cs207-FinalProject",
     install_requires=['numpy'],
+    python_requires='>=3.6',
 )
